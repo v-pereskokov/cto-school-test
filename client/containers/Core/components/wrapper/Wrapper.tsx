720456f4a7ec9c5306483c222b72f77234fde925
Vladislav Pereskokov <v-pereskokov@yandex-team.ru>
2020-08-12 19:16:56 +0300

    add yandex metrika

--- client/containers/Core/components/wrapper/Wrapper.tsx
+++ client/containers/Core/components/wrapper/Wrapper.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Switch} from 'react-router';
+import {YMInitializer} from 'react-yandex-metrika';
 
 import Layout from '__components/Layout';
 import {getRoutes} from '__utils/routes/makeRoutes';
@@ -7,11 +8,26 @@ import {getRoutes} from '__utils/routes/makeRoutes';
 import '__containers/Weather';
 import '__containers/NotFound';
 
+const YA_METRIKA_OPTIONS: {
+    accounts: number[];
+    options: object;
+    version: '1' | '2' | undefined;
+} = {
+    accounts: [66338728],
+    options: {
+        webvisor: true,
+    },
+    version: '2',
+};
+
 const Wrapper = () => {
     return (
-        <Layout>
-            <Switch children={getRoutes()}/>
-        </Layout>
+        <>
+            <Layout>
+                <Switch children={getRoutes()}/>
+            </Layout>
+            <YMInitializer {...YA_METRIKA_OPTIONS}/>
+        </>
     );
 };
 export default Wrapper;
